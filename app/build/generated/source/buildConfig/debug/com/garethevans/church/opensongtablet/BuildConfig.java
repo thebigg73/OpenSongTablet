d3ac349fa4d357075ee3a71c47d35135c617bfc2
Eberhard Beilharz <eb1@sil.org>
2019-01-19 21:52:32 +0100

    Upgrade gradle

--- app/build/generated/source/buildConfig/debug/com/garethevans/church/opensongtablet/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/garethevans/church/opensongtablet/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.garethevans.church.opensongtablet";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 153;
-  public static final String VERSION_NAME = "4.3.1";
+  public static final int VERSION_CODE = 154;
+  public static final String VERSION_NAME = "4.3.2";
 }
