3bdc548a2de6e65de7d03b5173d8aa1b6a1efdb2
thebigg73 <garethevans73.ge@gmail.com>
2021-04-04 23:07:39 +0100

    Update to V5.3.2

--- app/build/generated/source/buildConfig/debug/com/garethevans/church/opensongtablet/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/garethevans/church/opensongtablet/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.garethevans.church.opensongtablet";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 191;
-  public static final String VERSION_NAME = "5.3.1";
+  public static final int VERSION_CODE = 192;
+  public static final String VERSION_NAME = "5.3.2";
 }
