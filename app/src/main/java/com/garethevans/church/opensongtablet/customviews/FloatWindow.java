c92e85fc1f791e58d713a3ac4e1eb532d5a12876
thebigg73 <garethevans73.ge@gmail.com>
2021-04-14 20:23:28 +0100

    Got sticky notes working

---
+++ app/src/main/java/com/garethevans/church/opensongtablet/customviews/FloatWindow.java
@@ -0,0 +1,29 @@
+package com.garethevans.church.opensongtablet.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class FloatWindow extends LinearLayout {
+
+
+    public FloatWindow(Context context) {
+        super(context);
+    }
+
+    public FloatWindow(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FloatWindow(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+}
