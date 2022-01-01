9b5d39cc042a51cd352bbb9df78401b05a6a6ee9
thebigg73 <garethevans73.ge@gmail.com>
2022-01-01 23:04:52 +0000

    Updated views in Nearby connections

--- app/src/main/java/com/garethevans/church/opensongtablet/customviews/MaterialRadioButtonItem.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/customviews/MaterialRadioButtonItem.java
@@ -7,30 +7,28 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.garethevans.church.opensongtablet.R;
-import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.radiobutton.MaterialRadioButton;
 import com.google.android.material.textview.MaterialTextView;
 
-public class MaterialSwitch extends LinearLayout {
+public class MaterialRadioButtonItem extends LinearLayout {
 
-    private final MaterialTextView textView, hintView;
-    private final SwitchMaterial switchMaterial;
+    private final MaterialRadioButton radioButton;
+    private final MaterialTextView textView;
+    private final MaterialTextView hintView;
 
-    public MaterialSwitch(@NonNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
+    public MaterialRadioButtonItem(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
-        inflate(context, R.layout.view_material_switch, this);
-
+        inflate(context, R.layout.view_material_radiobutton,this);
+        radioButton = findViewById(R.id.radioButton);
         textView = findViewById(R.id.textView);
         hintView = findViewById(R.id.hintView);
-        switchMaterial = findViewById(R.id.materialSwitch);
 
         textView.setId(View.generateViewId());
         hintView.setId(View.generateViewId());
-        switchMaterial.setId(View.generateViewId());
+        radioButton.setId(View.generateViewId());
 
         int[] set = new int[] {android.R.attr.text, android.R.attr.hint, android.R.attr.checked};
         TypedArray typedArray = context.obtainStyledAttributes(attrs,set);
@@ -41,58 +39,45 @@ public class MaterialSwitch extends LinearLayout {
         String hintText = typedArray.getString(1);
         setHint(hintText);
 
-        boolean isSwitched = typedArray.getBoolean(2,false);
-        setChecked(isSwitched);
+        boolean checked = typedArray.getBoolean(2,false);
+        setChecked(checked);
 
         typedArray.recycle();
     }
 
     public void setText(String text) {
-        textView.setText(text);
         if (text==null || text.isEmpty()) {
             textView.setVisibility(View.GONE);
         } else {
             textView.setVisibility(View.VISIBLE);
         }
+        textView.setText(text);
     }
 
     public void setHint(String hint) {
-        hintView.setText(hint);
         if (hint==null || hint.isEmpty()) {
             hintView.setVisibility(View.GONE);
         } else {
             hintView.setVisibility(View.VISIBLE);
         }
+        hintView.setText(hint);
     }
 
-    public void setChecked(boolean switchedOn) {
-        switchMaterial.setChecked(switchedOn);
+    public void setChecked(boolean checked) {
+        radioButton.setChecked(checked);
     }
 
-    public boolean getChecked() {
-        return switchMaterial.isChecked();
-    }
-
-    public SwitchMaterial getSwitch() {
-        return switchMaterial;
+    public boolean isChecked() {
+        return radioButton.isChecked();
     }
 
     public void setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener onCheckedChangeListener) {
-        switchMaterial.setOnCheckedChangeListener(onCheckedChangeListener);
+        radioButton.setOnCheckedChangeListener(onCheckedChangeListener);
     }
 
     public void setEnabled(boolean enabled) {
         textView.setEnabled(enabled);
         hintView.setEnabled(enabled);
-        switchMaterial.setEnabled(enabled);
-        float alpha;
-        if (enabled) {
-            alpha = 1.0f;
-        } else {
-            alpha = 0.5f;
-        }
-        textView.setAlpha(alpha);
-        hintView.setAlpha(alpha);
-        switchMaterial.setAlpha(alpha);
+        radioButton.setEnabled(enabled);
     }
 }
--- app/src/main/java/com/garethevans/church/opensongtablet/nearby/NearbyConnectionsFragment.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/nearby/NearbyConnectionsFragment.java
@@ -73,10 +73,10 @@ public class NearbyConnectionsFragment extends Fragment {
         }
 
         // IV - Display relevant options to process nearby Song Section changes and autoscroll
-        if (mainActivityInterface.getMode().equals("Performance")) {
-            myView.receiveHostSections.setVisibility(View.GONE);
-        } else {
-            myView.receiveAutoscroll.setVisibility(View.GONE);
+        if (mainActivityInterface.getMode().equals("Presenter")) {
+            // This will work in Stage and Perfomance Mode
+            // As will sections (if using pdf pages)
+            myView.receiveAutoscroll.setEnabled(false);
         }
 
         // Set the host switches
@@ -153,6 +153,8 @@ public class NearbyConnectionsFragment extends Fragment {
                 mainActivityInterface.getNearbyConnections().stopDiscovery(requireContext());
                 mainActivityInterface.getNearbyConnections().stopAdvertising(requireContext());
                 mainActivityInterface.getNearbyConnections().turnOffNearby(requireContext());
+                myView.connectionsHost.setChecked(false);
+                myView.connectionsClient.setChecked(false);
             }
         });
         myView.connectionsHost.setOnCheckedChangeListener((radioButton, isChecked) -> {
@@ -162,6 +164,8 @@ public class NearbyConnectionsFragment extends Fragment {
                 offHostClient(true,false);
                 mainActivityInterface.getNearbyConnections().stopDiscovery(requireContext());
                 mainActivityInterface.getNearbyConnections().startAdvertising(requireContext(),mainActivityInterface);
+                myView.connectionsOff.setChecked(false);
+                myView.connectionsClient.setChecked(false);
             }
         });
         myView.connectionsClient.setOnCheckedChangeListener((radioButton, isChecked) -> {
@@ -173,7 +177,8 @@ public class NearbyConnectionsFragment extends Fragment {
                 // IV - Short delay to help stability
                 Handler h = new Handler();
                 h.postDelayed(() -> myView.searchForHosts.performClick(),2000);
-
+                myView.connectionsOff.setChecked(false);
+                myView.connectionsHost.setChecked(false);
             } else {
                 // IV - Reset the client options when leaving client mode
                 mainActivityInterface.getNearbyConnections().setReceiveHostFiles(false);
@@ -195,16 +200,12 @@ public class NearbyConnectionsFragment extends Fragment {
                 myView.searchForHosts.setEnabled(false);
                 myView.searchForHosts.setText(getString(R.string.connections_searching));
                 mainActivityInterface.getNearbyConnections().startDiscovery(requireContext(),mainActivityInterface);
-                Handler h = new Handler();
-                h.postDelayed(() -> {
-                    // Because there is a delay, check the fragment with the view is still available
-                    if (myView.searchForHosts!=null) {
-                        try {
-                            myView.searchForHosts.setText(getString(R.string.connections_discover));
-                            myView.searchForHosts.setEnabled(true);
-                        } catch (Exception e) {
-                            Log.d("OptionMenu","Lost reference to discovery button");
-                        }
+                myView.searchForHosts.postDelayed(() -> {
+                    try {
+                        myView.searchForHosts.setText(getString(R.string.connections_discover));
+                        myView.searchForHosts.setEnabled(true);
+                    } catch (Exception e) {
+                        Log.d("OptionMenu","Lost reference to discovery button");
                     }
                 },10000);
             }
@@ -233,119 +234,3 @@ public class NearbyConnectionsFragment extends Fragment {
     }
 
 }
-
-
-/*package com.garethevans.church.opensongtablet;
-
-        import android.app.Dialog;
-        import android.content.Context;
-        import android.content.DialogInterface;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.view.Window;
-        import android.widget.EditText;
-        import android.widget.TextView;
-
-        import androidx.annotation.NonNull;
-        import androidx.fragment.app.DialogFragment;
-
-        import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class PopUpConnectFragment extends DialogFragment {
-
-    static PopUpConnectFragment newInstance() {
-        PopUpConnectFragment frag;
-        frag = new PopUpConnectFragment();
-        return frag;
-    }
-
-    public interface MyInterface {
-        void prepareOptionMenu();
-    }
-
-    private static MyInterface mListener;
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        mListener = (MyInterface) context;
-        super.onAttach(context);
-    }
-
-    private TextView title, deviceNameTextView;
-    private EditText deviceNameEditText;
-    private FloatingActionButton saveMe;
-    private Preferences preferences;
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (savedInstanceState != null) {
-            this.dismiss();
-        }
-        if (getDialog() == null) {
-            dismiss();
-        }
-
-        getDialog().setCanceledOnTouchOutside(true);
-        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        View V = inflater.inflate(R.layout.popup_connect, container, false);
-
-        // Set the title based on the whattodo
-
-        title = V.findViewById(R.id.dialogtitle);
-        title.setText(getString(R.string.connections_connect));
-        final FloatingActionButton closeMe = V.findViewById(R.id.closeMe);
-        closeMe.setOnClickListener(view -> {
-            CustomAnimations.animateFAB(closeMe, getContext());
-            closeMe.setEnabled(false);
-            doSave();
-        });
-        saveMe = V.findViewById(R.id.saveMe);
-
-        preferences = new Preferences();
-
-        // Initialise the views
-        deviceNameEditText = V.findViewById(R.id.deviceNameEditText);
-        deviceNameTextView = V.findViewById(R.id.deviceNameTextView);
-        // IV - Changed to DeviceId which is the pref used by getUserNickname()
-        deviceNameEditText.setText(preferences.getMyPreferenceString(getContext(), "deviceId", ""));
-
-        // Set up save/tick listener
-        saveMe.setOnClickListener(view -> {
-            String s = deviceNameEditText.getText().toString().trim();
-            if (s.length() > 0) {
-                preferences.setMyPreferenceString(getContext(), "deviceId", s);
-                StaticVariables.deviceName = s;
-            }
-            doSave();
-        });
-        Dialog dialog = getDialog();
-        if (dialog!=null && getContext()!=null) {
-            PopUpSizeAndAlpha.decoratePopUp(getActivity(),dialog, preferences);
-        }
-        return V;
-    }
-
-    private void doSave() {
-        if (mListener!=null) {
-            mListener.prepareOptionMenu();
-        }
-        try {
-            dismiss();
-        } catch (Exception e) {
-            Log.d("d","Error closing fragment");
-        }
-    }
-
-    @Override
-    public void onCancel(@NonNull DialogInterface dialog) {
-        try {
-            this.dismiss();
-        } catch (Exception e) {
-            Log.d("d","Error closing the fragment");
-        }
-    }
-}*/
