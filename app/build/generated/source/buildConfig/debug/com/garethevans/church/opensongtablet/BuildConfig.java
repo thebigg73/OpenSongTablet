ea59c1f73233c932c2f8e2228c12ea322eda8ead
thebigg73 <garethevans73.ge@gmail.com>
2022-02-13 22:23:40 +0000

    Highlighter scaling

--- app/build/generated/source/buildConfig/debug/com/garethevans/church/opensongtablet/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/garethevans/church/opensongtablet/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.garethevans.church.opensongtablet";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 205;
-  public static final String VERSION_NAME = "5.4.5";
+  public static final int VERSION_CODE = 206;
+  public static final String VERSION_NAME = "5.4.6";
 }
