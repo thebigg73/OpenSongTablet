d02d08421d1b8261ac16aeee43419944c8a8f00a
iv-gha <68599243+iv-gha@users.noreply.github.com>
2022-04-07 10:49:57 +0100

    Enh: Set View SaveMe opens the first song IF the current song is not in the set.  When in a set, this restores ability to re-order songs without change of song.

--- app/src/main/java/com/garethevans/church/opensongtablet/PopUpSetViewNew.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/PopUpSetViewNew.java
@@ -270,8 +270,8 @@ public class PopUpSetViewNew extends DialogFragment {
         FloatingActionButton saveMe = V.findViewById(R.id.saveMe);
         saveMe.setOnClickListener(view -> {
             PopUpSetViewNew.this.doSave();
-            // IV - Display the first song
-            if (StaticVariables.mSetList!=null && StaticVariables.mSetList.length>0) {
+            // IV - If we have a set and the current song is not in the set - Display the first song
+            if (StaticVariables.mSetList!=null && StaticVariables.mSetList.length>0 && !setActions.isSongInSet(getContext(), preferences)) {
                 setActions.prepareFirstItem(getContext());
                 // IV - Avoid double refresh for Presentation mode, see OnDismiss()
                 if (!StaticVariables.whichMode.equals("Presentation")) {
