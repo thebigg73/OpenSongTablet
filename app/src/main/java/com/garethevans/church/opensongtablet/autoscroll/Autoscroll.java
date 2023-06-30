f9dd9204dfc15021aee0ca6f3c72f50dfd8f6e28
Gareth Evans <garethevans73.ge@gmail.com>
2023-06-30 20:51:24 +0100

    Changed gravity of autoscroll bottom sheet text

--- app/src/main/java/com/garethevans/church/opensongtablet/autoscroll/Autoscroll.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/autoscroll/Autoscroll.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
