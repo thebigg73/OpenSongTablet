e491ba8104a39d84159f60c2261d0d1a1860ff45
Gareth Evans <garethevans73.ge@gmail.com>
2023-12-11 21:01:00 +0000

    Check for locale loading

--- app/src/main/java/com/garethevans/church/opensongtablet/appdata/FixLocale.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/appdata/FixLocale.java
@@ -9,6 +9,10 @@ import java.util.Locale;
 
 public class FixLocale {
 
+    // By default the app runs in the device language (if available)
+    // English is the fall back default
+    // If a user chooses a different language in the app, this is used regardless of the device settings
+
     @SuppressWarnings({"unused","FieldCanBeLocal"})
     private final String TAG = "FixLocale";
     private Locale userLocale;
@@ -19,11 +23,12 @@ public class FixLocale {
 
     public void setLocale(Context c, MainActivityInterface mainActivityInterface) {
         // Locale
+        boolean wasset = false;
         try {
             // Get the user's preference
             String val = mainActivityInterface.getPreferences().getMyPreferenceString("language",null);
 
-            // If this is already set, that' what we will use
+            // If this is already set, that's what we will use
             if (val!=null) {
                 userLocale = new Locale(val);
 
@@ -33,7 +38,7 @@ public class FixLocale {
                 String deviceval = Locale.getDefault().getLanguage();
 
                 // If this is supported, set the user preference, otherwise use English
-                String translations = "af cs de el es fr hu it ja pl pt ru sr sv zh";
+                String translations = "af cs de el es fr hu it ja pl pt ru sr sv uk zh";
                 if (!translations.contains(deviceval)) {
                     deviceval = "en";
                 }
@@ -49,10 +54,21 @@ public class FixLocale {
             Locale.setDefault(userLocale);
             configuration.setLocale(userLocale);
             c.getResources().updateConfiguration(configuration, c.getResources().getDisplayMetrics());
-
+            wasset = true;
         } catch (Exception e) {
             e.printStackTrace();
             userLocale = new Locale("en");
+        }
+
+        if (!wasset) {
+            try {
+                Configuration configuration = new Configuration();
+                Locale.setDefault(userLocale);
+                configuration.setLocale(userLocale);
+                c.getResources().updateConfiguration(configuration, c.getResources().getDisplayMetrics());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
