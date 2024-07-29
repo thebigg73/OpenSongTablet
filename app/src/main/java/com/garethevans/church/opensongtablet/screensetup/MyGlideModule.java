00e1fc091a39d6193732383f27b1fa5bf430874e
Gareth Evans <garethevans73.ge@gmail.com>
2024-07-29 11:45:30 +0100

    Added GlideApp module (appears redundant, but stops log errors)

---
+++ app/src/main/java/com/garethevans/church/opensongtablet/screensetup/MyGlideModule.java
@@ -0,0 +1,8 @@
+package com.garethevans.church.opensongtablet.screensetup;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@SuppressWarnings("unused")
+@GlideModule
+public class MyGlideModule extends AppGlideModule {}
