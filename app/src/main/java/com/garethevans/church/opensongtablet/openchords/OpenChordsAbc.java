069431343fa5ac33631c00372833e589a6115998
Gareth Evans <garethevans73.ge@gmail.com>
2025-03-12 22:03:35 +0000

    Potential to use OpenChordsAPI to convert MusicXML to abc notation

---
+++ app/src/main/java/com/garethevans/church/opensongtablet/openchords/OpenChordsAbc.java
@@ -0,0 +1,13 @@
+package com.garethevans.church.opensongtablet.openchords;
+
+public class OpenChordsAbc {
+
+    private String abc;
+
+    public String getAbc() {
+        return abc;
+    }
+    public void setAbc(String abc) {
+        this.abc = abc;
+    }
+}
---
+++ app/src/main/java/com/garethevans/church/opensongtablet/openchords/OpenChordsMusicXML.java
@@ -0,0 +1,12 @@
+package com.garethevans.church.opensongtablet.openchords;
+
+public class OpenChordsMusicXML {
+    private String musicxml;
+
+    public String getMusicxml() {
+        return musicxml;
+    }
+    public void setMusicxml(String musicxml) {
+        this.musicxml = musicxml;
+    }
+}
