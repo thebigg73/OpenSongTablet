5386d5fb516b9ddf2fc5053495bd606e5fd5dcb5
Gareth Evans <garethevans73.ge@gmail.com>
2025-10-29 19:07:27 +0000

    Adjustment of piano chord fingering colouring the better suit colour deficiency issues

--- app/src/main/java/com/garethevans/church/opensongtablet/chords/ChordFingeringBottomSheet.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/chords/ChordFingeringBottomSheet.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -218,7 +217,6 @@ public class ChordFingeringBottomSheet extends BottomSheetCommon {
     private void showProgress(boolean show) {
         if (myView!=null) {
             myView.getRoot().post(() -> {
-                Log.d(TAG,"showing progress bar:"+show);
                 myView.progressBar.setVisibility(show ? View.VISIBLE : View.GONE);
             });
         }
