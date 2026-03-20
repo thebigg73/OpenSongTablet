c3fe7379e5c6ad0b0da61e3bf54fb77c5deda147
thebigg73 <garethevans73.ge@gmail.com>
2026-03-20 17:45:13 +0000

    v6.6.5 (less the folder creation update)

--- app/src/main/java/com/garethevans/church/opensongtablet/__TODO__.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/__TODO__.java
@@ -6,6 +6,8 @@ public class __TODO__ {
 
 Add code issues here to check
 
-
+// Can't create a song folder if file exists with same name
+// Check OpenChords updates with read only settings
+// Implement ktor messaging system
 */
 }
