4dc6d46748f3cfc2e13a727b9a5ed4cf96389e07
thebigg73 <garethevans73.ge@gmail.com>
2026-03-22 21:32:52 +0000

    Updated translations

--- app/src/main/java/com/garethevans/church/opensongtablet/__TODO__.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/__TODO__.java
@@ -6,8 +6,5 @@ public class __TODO__ {
 
 Add code issues here to check
 
-// Can't create a song folder if file exists with same name
-// Check OpenChords updates with read only settings
-// Implement ktor messaging system
 */
 }
