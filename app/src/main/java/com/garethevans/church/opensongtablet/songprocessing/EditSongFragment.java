a82e49191c6ef5dc6396c3d8623a44304444a438
thebigg73 <garethevans73.ge@gmail.com>
2026-05-09 12:26:09 +0100

    Make sure double clicking the save doesn't crash the app!

--- app/src/main/java/com/garethevans/church/opensongtablet/songprocessing/EditSongFragment.java
+++ app/src/main/java/com/garethevans/church/opensongtablet/songprocessing/EditSongFragment.java
@@ -248,7 +248,11 @@ public class EditSongFragment extends Fragment implements EditSongFragmentInterf
 
             if (oktoproceed && mainActivityInterface.getSaveSong().doSave(mainActivityInterface.getTempSong())) {
                 // If successful, go back to the home page.  Otherwise stay here and await user decision from toast
-                mainActivityInterface.getMainHandler().post(() -> mainActivityInterface.navHome());
+                mainActivityInterface.getMainHandler().post(() -> {
+                    if (mainActivityInterface!=null) {
+                        mainActivityInterface.navHome();
+                    }
+                });
             } else if (oktoproceed) {
                 mainActivityInterface.getMainHandler().post(() -> mainActivityInterface.getShowToast().doIt(not_saved_string));
             }
