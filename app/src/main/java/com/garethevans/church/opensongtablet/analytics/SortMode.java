0ab0806cd5141beda993adf247d558f595809e7e
thebigg73 <garethevans73.ge@gmail.com>
2026-06-14 22:01:33 +0100

    Sort options for Analytics

---
+++ app/src/main/java/com/garethevans/church/opensongtablet/analytics/SortMode.java
@@ -0,0 +1,11 @@
+package com.garethevans.church.opensongtablet.analytics;
+
+public class SortMode {
+    public static final String POPULARITY = "popularity";
+    public static final String LAST_VIEWED = "last_viewed";
+    public static final String LAST_CAST = "last_cast";
+    public static final String LAST_SET = "last_added_to_set";
+    public static final String COUNT_SET = "count_set";
+    public static String lastSortMethod = POPULARITY;
+
+}
